02c41720a86979990172fdd5c492a3943437cc1f
Marius Ngaboyamahina <mngaboyamahina@cardinalpeak.com>
2020-11-23 00:46:07 +0200

    changes in visual

--- visual.py
+++ visual.py
@@ -3,6 +3,12 @@
 
 
 import pandas as pd
-a = pd.read_csv("law-firm-details.csv")
-a.to_html("law-firm-details.html")
+a = pd.read_csv("law-firm.csv")
+a.to_html("law-firm.html")
+
+# To generate aw-firm-details table, please un-comment those two line below (line 12 and 13).
+# But You will need to comment out line 6 and 7
+
+# a = pd.read_csv("law-firm-details.csv")
+# a.to_html("law-firm-details.html")
 html_file = a.to_html()
